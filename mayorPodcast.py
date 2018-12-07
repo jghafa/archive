db51e6025cb4df0a759b41eb93c828767625b992
jghafa <james.haley@yahoo.com>
2018-12-07 11:15:58 -0500

    Upload Podcasts

---
+++ mayorPodcast.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+"""
+Upload SoundCloud mp3 files to IA
+
+1) Get the files from \\san\gov\office of the mayor\office of the mayor\PIO\RadioSpots
+2) Copy to a spot accessable fro here, like /media/smb/TestDVD/MayorAudio/
+3) Make sure the file modification dates are correct for IA
+4) Run mayorAudioFiles.py
+"""
+
+#import shutil
+import glob
+import os
+import datetime
+from internetarchive import *
+
+PATH = '/media/smb/TestDVD/MayorAudio/'
+
+
+#Name of the Internet Archive collection target for uploads
+#CollectionName = 'test_collection'
+CollectionName = 'mayoraudiofiles'
+
+# This metadata media type
+vidmediatype = 'audio'
+
+# Title of the item in the collection.  This is the one people see.
+Title = ''
+
+#Unique indentifer for the upload, becomes the IA directory name
+Identifier = ''
+
+#Vidio date, used for the metadata, formated ISO 8601
+# yyyy-mm-dd
+vidDate = ''
+
+#video description, used for metadata, from spreadsheet via text file
+vidDesc = "Mayor's Podcasts"
+
+#metadata for the producer
+vidCreator = 'City of Fort Wayne, Indiana'
+
+#metadata for the Creative Commons license
+vidLicense = 'http://creativecommons.org/licenses/by-nc-sa/4.0/'
+
+#metadata for search fields
+vidSubject = ['Fort Wayne','Local Government','Mayor','Podcast']
+
+
+for file_name in glob.glob(PATH + '*.[mM][pP][3]'):
+    try:
+        mtime = os.path.getmtime(file_name)
+    except OSError:
+        print ('Not uploading ',file_name)
+        continue
+    last_modified_date = datetime.datetime.fromtimestamp(mtime)
+
+    newname = 'Podcast '+file_name.split('/')[-1]
+    Identifier = (newname.replace(',','-').
+                          replace(' - ','-').
+                          replace('- ','-').
+                          replace(';','-').
+                          replace(' ','-').
+                          replace('---','-').
+                          replace('--','-').
+                          replace('--','-').
+                          replace("'",'').
+                          replace('.mp3','')  )
+
+    vidDate = datetime.datetime.strftime(last_modified_date,'%Y-%m-%d')
+    Title = newname.split('.')[0].strip()
+    
+    md = dict(collection = CollectionName, 
+              title      = Title,
+              mediatype  = vidmediatype, 
+              description= vidDesc,
+              creator    = vidCreator,
+              subject    = vidSubject,
+              licenseurl = vidLicense, 
+              date       = vidDate)
+
+    try:
+        r = upload(Identifier, files=file_name, metadata=md, 
+                   retries=30, checksum=True) #retries_sleep=20,
+        print ('Status code', r[0].status_code, Identifier)
+    except Exception as e:
+        print ('Failed on ', Identifier, e.message, e.args)
